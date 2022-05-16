f06ce5cccf3aa4dc9d727fad570f6e2f5bbe71cc
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-05-16 22:27:05 +0200

    ➕💄 Front page + sveltekit-svg for svg components

---
+++ src/lib/OrgMemberCard/OrgMemberCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let image: string;
+	export let name: string;
+	export let role: string;
+</script>
+
+<div class="cardAsso">
+	<img src={image} alt={name} class="cardAsso__image" />
+	<div class="cardAsso__content">
+		<h3 class="cardAsso__content__name">{name}</h3>
+		<p class="cardAsso__content__role">{role}</p>
+	</div>
+</div>
+
+<style lang="scss">
+	@import './OrgMemberCard';
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,67 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import OrgMemberCard from '$lib/OrgMemberCard/OrgMemberCard.svelte';
 
+	import Scroll from '../assets/scroll.svg';
+	import Illustration from '../assets/illustration.svg';
+	import Separator_1 from '../assets/separator-1.svg';
+	import Separator_2 from '../assets/separator-2.svg';
+</script>
 
-<style lang="scss"> 
-	p {
-		& a {
-			color: green;
-		}
-	}
+<section class="landing">
+	<div class="landing__title">
+		<h1>Bienvenue sur le site de la Licence Bio—MAD</h1>
+	</div>
+	<div class="scrollDown">
+		<Scroll />
+	</div>
+	<div class="separator">
+		<Separator_1 />
+	</div>
+</section>
+<section class="about">
+	<h2 class="about__title">Une licence en 2 parties</h2>
+	<div class="about__content">
+		<p class="about__content__text">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus tellus imperdiet tellus
+			pharetra. Vulputate tempor volutpat feugiat aliquet vestibulum cras ultricies ut eu. Amet
+			aenean dapibus bibendum in pellentesque nisi sapien interdum. Consequat ut congue iaculis
+			varius at tincidunt pretium sed pellentesque. Eget eget diam faucibus nulla tempus risus,
+			mauris, viverra nulla. In id neque bibendum quam lectus. Orci arcu, nulla at nibh amet
+			rhoncus, feugiat eleifend. Vivamus odio quis nunc, consequat et pretium sed augue. Felis sit
+			risus in aliquam senectus fames cursus. Enim, duis donec ultricies sed. Egestas ornare
+			facilisis sem sed eu viverra eros ornare. Ornare placerat sagittis viverra at rutrum.
+		</p>
+		<div class="about__content__illustration">
+			<Illustration />
+		</div>
+	</div>
+</section>
+<section class="albm">
+	<div class="separator">
+		<Separator_2 />
+	</div>
+	<h2 class="albm__title">L’association :</h2>
+	<div class="albm__content">
+		<img src="/images/logo_albm.webp" alt="Logo ALBM" />
+		<div class="albm__content__container">
+			<div class="albm__content__container__item">
+				<p class="albm__content__text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rhoncus tellus imperdiet tellus
+					pharetra. Vulputate tempor volutpat feugiat aliquet vestibulum cras ultricies ut eu. Amet
+					aenean dapibus bibendum in pellentesque nisi sapien interdum. Consequat ut congue iaculis
+					varius at tincidunt pretium sed pellentesque. Eget eget diam faucibus nulla tempus risus,
+					mauris, viverra nulla.
+				</p>
+			</div>
+			<div class="albm__content__container__item">
+				<OrgMemberCard name="Tanguy BERNARD" role="Président" image="/images/pp.png" />
+				<OrgMemberCard name="Léo MEYER" role="Vice-président" image="/images/pp.png" />
+				<OrgMemberCard name="Enora CORRE" role="Secrétaire" image="/images/pp.png" />
+			</div>
+		</div>
+	</div>
+</section>
+
+<style lang="scss">
+	@import './index';
 </style>
