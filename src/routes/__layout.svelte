d43c1f6232a62ded37bfa6fe5c8071f871ec69b7
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-05-17 13:10:50 +0200

    💄 Footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+	import Footer from '$lib/footer/Footer.svelte';
 	import Header from '../lib/header/Header.svelte';
 </script>
 
 <Header />
 
 <slot />
+
+<Footer />
 
 <style global lang="scss">
 	@use '../app.scss';
