1c4fdd7525266b6b360f1fc30f0118442acc0db7
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2022-05-26 23:00:38 +0200

    ➖ Remove sveltekit-svg

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import svg from '@poppanator/sveltekit-svg';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,9 +9,7 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		vite: {
-			plugins: [svg()]
-		}
+		vite: {}
 	}
 };
 
