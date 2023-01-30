a70365e0d5192739e2ed02b346344d2f1e3e1422
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-01-30 23:36:38 +0100

    feat(components): add Textarea primitive

---
+++ src/lib/primitives/Textarea/Textarea.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import autosize from 'svelte-autosize';
+
+	export let value: string;
+	export let id = '';
+</script>
+
+<div class="input-field">
+	<textarea {...$$restProps} bind:value id={id} use:autosize/>
+	<label class:active={value} for={id}>
+		<slot/>
+	</label>
+	{#if $$slots.helper}
+		<span class="helper-text" data-error="wrong" data-success="right">
+			<slot name="helper"/>
+		</span>
+	{/if}
+</div>
+
+<style lang="scss">
+  @import 'Textarea';
+</style>
