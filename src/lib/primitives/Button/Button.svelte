45230b0a7e4f4520f65ee209313d369b4652c98a
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-01-30 23:35:11 +0100

    feat(components): add Button primitive
    feat(components): fix Button style import

---
+++ src/lib/primitives/Button/Button.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let className = '';
+	export let color = '';
+	export let hover = false;
+
+	let colorClass: string;
+
+	if ($$slots.icon) {
+		className += ' btn-grid';
+	}
+
+	if (hover) {
+		className += ` btn-hover`;
+	}
+
+	$: colorClass = color ? ` btn-${color}` : '';
+</script>
+
+<button {...$$restProps} class={className.trim() + colorClass} on:click>
+	{#if $$slots.icon}
+		<span class="icon"><slot name="icon"/></span>
+	{/if}
+	<span><slot/></span>
+</button>
+
+<style lang="scss">
+  @import "Button";
+</style>
