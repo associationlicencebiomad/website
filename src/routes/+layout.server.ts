0c9d73c026bf4a8efefe5c8af37902a3d77c3f44
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-01-31 13:44:32 +0100

    feat(auth): working login form

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import type {LayoutServerLoad} from './$types';
 import {getServerSession} from '@supabase/auth-helpers-sveltekit';
 
 export const load: LayoutServerLoad = async (event) => {
-	event.depends('auth:session_info')
+	event.depends('supabase:auth')
 	const {loggedInUser} = event.locals
 	return {
 		session: await getServerSession(event),
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-	import { supabaseClient } from '$lib/db';
-	import { invalidate } from '$app/navigation';
-	import { onMount } from 'svelte';
+	import {supabaseClient} from '$lib/db';
+	import {invalidate} from '$app/navigation';
+	import {onMount} from 'svelte';
+
+	import '../app.scss';
 
 	onMount(() => {
 		const {
-			data: { subscription }
+			data: {subscription}
 		} = supabaseClient.auth.onAuthStateChange(() => {
 			invalidate('supabase:auth');
 		});
@@ -16,4 +18,12 @@
 	});
 </script>
 
-<slot />
+<svelte:head>
+	<title>ALBM</title>
+</svelte:head>
+
+<slot/>
+
+<!--<style global lang="scss">-->
+<!--  @use '../app.scss';-->
+<!--</style>-->
