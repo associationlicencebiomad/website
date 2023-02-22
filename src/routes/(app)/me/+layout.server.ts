1f06e54c7bdce0cf7752eb85d9fe8cedd51aae8a
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-02-22 13:56:54 +0100

    feat(security): Guard profile edition routes

---
+++ src/routes/(app)/me/+layout.server.ts
@@ -0,0 +1,10 @@
+import type {LayoutServerLoad} from './$types'
+import {redirect} from "@sveltejs/kit";
+import {getSupabase} from "@supabase/auth-helpers-sveltekit";
+
+export const load: LayoutServerLoad = async (event) => {
+	const {session} = await getSupabase(event)
+	if (!session) {
+		throw redirect(303, '/login')
+	}
+}
