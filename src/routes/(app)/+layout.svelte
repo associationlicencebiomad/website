fc5fde9bd3da6d3e59798504d4e48a4cf9a57561
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-04-27 14:31:39 +0200

    feat(notifications): update packages + notification system

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,8 +1,19 @@
 <script>
-	import Footer from "$lib/components/Footer/Footer.svelte";
-	import Header from "$lib/components/Header/Header.svelte";
+    import Notifications from 'svelte-notifications';
+
+    import Footer from "$lib/components/Footer/Footer.svelte";
+    import Header from "$lib/components/Header/Header.svelte";
+    import NotificationCard from "$lib/components/Notifications/NotificationCard.svelte";
 </script>
 
-<Header/>
-<slot/>
-<Footer/>
+<Notifications item={NotificationCard}>
+    <Header/>
+    <slot/>
+    <Footer/>
+</Notifications>
+
+<style lang="scss">
+    :global(.position-bottom-right) {
+      right: 8px;
+    }
+</style>
