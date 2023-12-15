dc24e0be8106d17c74b9a963ce46644061bcd38f
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2023-12-15 17:33:50 +0100

    feat: SvelteKit 2

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,4 +1,4 @@
-import {persisted} from 'svelte-local-storage-store'
+import {persisted} from 'svelte-persisted-store'
 import {ThemeType} from "../types/theme.types";
 
 export const theme = persisted<ThemeType>('theme', ThemeType.light);
